93e9bdc36e4bdc9d09dfb0048a1c8fa48739eee2
David Hu <who.is.david101@gmail.com>
2025-01-24 00:05:38 -0500

    refactored dev branch changes in project structure

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,6 @@
 # Tests for database logic
 
-from app.database import get_db_connection
+from backend.database.database import get_db_connection
 
 def test_db_connection():
     conn = get_db_connection()
